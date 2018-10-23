dc2aa66bd9d980b9e9d3a32130478bc02d964503
julianedwards <julian.edwards@mongodb.com>
2018-10-23 17:03:07 -0400

    remove compiler trickes

--- collector_benchmark_test.go
+++ collector_benchmark_test.go
@@ -1,7 +1,6 @@
 package ftdc
 
 import (
-	"fmt"
 	"math/rand"
 	"testing"
 	"time"
@@ -26,14 +25,9 @@ func BenchmarkCollectorInterface(b *testing.B) {
 							collector.Add(test.docs[n%len(test.docs)])
 						}
 					})
-					var result []byte
 					b.Run("Resolve", func(b *testing.B) {
 						for n := 0; n < b.N; n++ {
-							r, _ := collector.Resolve()
-							result = r
-							if false {
-								fmt.Println(result)
-							}
+							collector.Resolve()
 						}
 					})
 				})
