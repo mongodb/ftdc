8d3dc89c54e0c12c43efaddfddf4e880fe94093b
Sam Kleinman <sam@10gen.com>
2018-12-14 16:46:40 -0500

    remove experimental matrix form

--- bson_matrix.go
+++ bson_matrix.go
@@ -4,41 +4,8 @@ import (
 	"io"
 
 	"github.com/mongodb/ftdc/bsonx"
-	"github.com/mongodb/mongo-go-driver/bson"
-	"github.com/mongodb/mongo-go-driver/bson/primitive"
 	"github.com/pkg/errors"
 )
-
-func rehydrateMatrixExperimental(metrics []Metric, sample int) (bson.E, int, error) {
-	if sample >= len(metrics) {
-		return bson.E{}, sample, io.EOF
-	}
-
-	// the bsonx library's representation of arrays is more
-	// efficent when constructing arrays from documents,
-	// otherwise.
-	key := metrics[sample].Key()
-	var vals interface{}
-	switch metrics[sample].originalType {
-	case bsonx.TypeBoolean, bsonx.TypeDouble, bsonx.TypeInt64, bsonx.TypeInt32, bsonx.TypeDateTime:
-		vals = metrics[sample].getSeries()
-	case bsonx.TypeTimestamp:
-		array := make(bson.A, 0, len(metrics[sample].Values))
-		for idx, p := range metrics[sample].Values {
-			array = append(array, primitive.Timestamp{
-				T: uint32(p),
-				I: uint32(metrics[sample+1].Values[idx]),
-			})
-		}
-		vals = array
-		sample++
-	default:
-		return bson.E{}, sample, errors.New("invalid data type")
-	}
-
-	sample++
-	return bson.E{Key: key, Value: vals}, sample, nil
-}
 
 func rehydrateMatrix(metrics []Metric, sample int) (*bsonx.Element, int, error) {
 	if sample >= len(metrics) {
