1bf4aaaed2d1a94154e60942cfbd5b426ac0de45
edzh <edwinzzhou@gmail.com>
2021-06-16 16:31:58 -0400

    test translation integration

--- t2.go
+++ t2.go
@@ -4,118 +4,75 @@ import (
 	"context"
 	"io"
 	"log"
-
+	"math"
 	"github.com/evergreen-ci/birch"
-
 	"github.com/pkg/errors"
 )
 
 const SECOND_MS int64 = 1000
+const MAX_SAMPLES = 300
 
-// returns last line of current window
-// this will handle if a second of metrics spans across multiple chunks
-// I don't think this happens though
-// finds possible end of a window in metric values
-/**
-[998, 998, 999, 999| 1000, 1000, 1001]
-								 ^    ^
-				  windowIdx  currentTime
-					999/1000 = 0
-					1000/1000 = 1, 1 > 0, so record the latest second windowIdx and currentTime
-	this might cause windows to span multiple seconds and intro gaps
-	find out if we need to handle when multiple seconds are in one chunk
-*/
-func window(timestamp Metric) int {
-	var currentTime int64 = 0
-	var windowIdx int = -1
-	for idx, ts := range timestamp.Values {
-		if ts/SECOND_MS > currentTime {
-			windowIdx = idx - 1
-			currentTime = ts / SECOND_MS
-		}
-	}
-
-	return windowIdx
-}
-
-/*
-{
-  "cedar": {
-    "Actor.Operation": {
-      "timestamp": 999, // non-cumulative, taken from ts
-      "n": 0            // cumulative, taken from counters.n
-      "ops": 0,         // cumulative, taken from counters.ops
-      "size": 0,        // cumulative, taken from counters.size
-      "errors": 0,      // cumulative, taken from counters.errors
-      "dur": 0,         // cumulative, taken from timers.dur
-      "total": 0        // cumulative, taken from timers.total
-    }
-  },
-  "start": 999,
-  "end": 1000
-}
-*/
-
-func CreateStats(ctx context.Context, iter *ChunkIterator, output io.Writer, actorOpName string) error {
-	collector := NewStreamingCollector(1000, output)
+func TranslateGenny(ctx context.Context, iter *ChunkIterator, output io.Writer, actorOpName string) error {
+	collector := NewStreamingCollector(MAX_SAMPLES, output)
 	defer FlushCollector(collector, output)
+	
+	currentSecond := int64(0)
+	endOfSecondIdx := -1
+	prevChunk := iter.Chunk()
 
 	for iter.Next() {
 		if err := ctx.Err(); err != nil {
 			return errors.New("operation aborted")
 		}
-		chunk := iter.Chunk()
-
-		timestamp := chunk.Metrics[0]
-		endOfWindowIdx := window(timestamp)
-
+		currChunk := iter.Chunk()
 		elems := make([]*birch.Element, 0)
 		var startTime *birch.Element
-		var endTime *birch.Element
 
-		if endOfWindowIdx > -1 {
-			for _, metric := range chunk.Metrics {
-				switch name := metric.Key(); name {
-				case "ts":
-					currentTimestamp := metric.Values[endOfWindowIdx]
-					element := birch.EC.DateTime("timestamp", currentTimestamp)
-					startTime = birch.EC.DateTime("start", currentTimestamp)
-					endTime = birch.EC.DateTime("end", metric.Values[endOfWindowIdx+1])
-					elems = append(elems, element)
-				case "counters.n":
-					elems = append(elems, birch.EC.Int64("n", metric.Values[endOfWindowIdx]))
-				case "counters.ops":
-					elems = append(elems, birch.EC.Int64("ops", metric.Values[endOfWindowIdx]))
-				case "counters.size":
-					elems = append(elems, birch.EC.Int64("size", metric.Values[endOfWindowIdx]))
-				case "counters.errors":
-					elems = append(elems, birch.EC.Int64("errors", metric.Values[endOfWindowIdx]))
-				case "timers.dur":
-					elems = append(elems, birch.EC.Int64("dur", metric.Values[endOfWindowIdx]))
-				case "timers.total":
-					elems = append(elems, birch.EC.Int64("total", metric.Values[endOfWindowIdx]))
-				default:
-					break
+		timestamp := currChunk.Metrics[0]
+
+		for idx, ts := range timestamp.Values {
+			if math.Ceil(float64(ts)/float64(SECOND_MS)) != float64(currentSecond) && endOfSecondIdx > -1 {
+				
+				chunk := currChunk
+				if endOfSecondIdx == len(prevChunk.Metrics[0].Values)-1 {
+					chunk = prevChunk
+				}
+
+				for _, metric := range chunk.Metrics {
+					switch name := metric.Key(); name {
+					case "ts":
+						startTime = birch.EC.DateTime("start", currentSecond*SECOND_MS)
+					case "counters.n":
+						elems = append(elems, birch.EC.Int64("n", metric.Values[endOfSecondIdx]))
+					case "counters.ops":
+						elems = append(elems, birch.EC.Int64("ops", metric.Values[endOfSecondIdx]))
+					case "counters.size":
+						elems = append(elems, birch.EC.Int64("size", metric.Values[endOfSecondIdx]))
+					case "counters.errors":
+						elems = append(elems, birch.EC.Int64("errors", metric.Values[endOfSecondIdx]))
+					case "timers.dur":
+						elems = append(elems, birch.EC.Int64("dur", metric.Values[endOfSecondIdx]))
+					case "timers.total":
+						elems = append(elems, birch.EC.Int64("total", metric.Values[endOfSecondIdx]))
+					case "gauges.workers":
+						elems = append(elems, birch.EC.Int64("workers", metric.Values[endOfSecondIdx]))
+					case "gauges.failed":
+						elems = append(elems, birch.EC.Int64("failed", metric.Values[endOfSecondIdx]))
+					default:
+						break
+					}
 				}
 			}
-		} else {
-			for _, metric := range chunk.Metrics {
-				switch name := metric.Key(); name {
-				case "ts":
-					startTime = birch.EC.DateTime("start", metric.Values[len(metric.Values)-2])
-					endTime = birch.EC.DateTime("end", metric.Values[len(metric.Values)-1])
-				default:
-					break
-				}
-			}
+			endOfSecondIdx = idx
+			currentSecond = int64(math.Ceil(float64(ts)/float64(SECOND_MS)))
+			prevChunk = currChunk
 		}
 
-		actorOpElems := birch.NewDocument(elems...)
-		actorOpDoc := birch.EC.SubDocument(actorOpName, actorOpElems)
-		cedarElems := birch.NewDocument(actorOpDoc, startTime, endTime)
-		cedarDoc := birch.EC.SubDocument("cedar", cedarElems)
-
 		if len(elems) > 0 {
+			actorOpElems := birch.NewDocument(elems...)
+			actorOpDoc := birch.EC.SubDocument(actorOpName, actorOpElems)
+			cedarElems := birch.NewDocument(startTime, actorOpDoc)
+			cedarDoc := birch.EC.SubDocument("cedar", cedarElems)
 			if err := collector.Add(birch.NewDocument(cedarDoc)); err != nil {
 				log.Fatal(err)
 				return errors.WithStack(err)
---
+++ t2_test.go
@@ -0,0 +1,77 @@
+package ftdc
+
+import (
+	"bytes"
+	"context"
+	"io/ioutil"
+	"os"
+	"testing"
+	"math/rand"
+	"github.com/stretchr/testify/require"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTranslateGennyIntegration(t *testing.T) {
+	for _, test := range []struct {
+		name 						string
+		path 						string
+		skipSlow        bool
+		skipAll         bool
+		expectedNum     int
+		expectedChunks  int
+		expectedMetrics int
+		reportInterval  int
+		docLen          int
+	}{
+		{
+			name: "GennyMock",
+			path: "genny_metrics.ftdc",
+			docLen: 4,
+			expectedNum: 9,
+			expectedChunks: 4,
+			expectedMetrics: 300,
+			reportInterval: 1000,
+			skipSlow: true,
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			file, err := os.Open(test.path)
+			require.NoError(t, err)
+			defer func() { printError(file.Close()) }()
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+			data, err := ioutil.ReadAll(file)
+			require.NoError(t, err)
+
+			t.Run("Translate", func(t *testing.T) {
+				iter := ReadChunks(ctx, bytes.NewBuffer(data))
+				out := &bytes.Buffer{}
+				err := TranslateGenny(ctx, iter, out, "test")
+				require.NoError(t, err)
+
+				// verify output
+				iter = ReadChunks(ctx, out)
+				counter := 0
+				num := 0
+				for iter.Next() {
+					c := iter.Chunk()
+					counter++
+					if num == 0 {
+						num = len(c.Metrics)
+						require.Equal(t, test.expectedNum, num)
+					}
+					metric := c.Metrics[rand.Intn(num)]
+					require.True(t, len(metric.Values) > 0)
+
+					assert.Equal(t, metric.startingValue, metric.Values[0], "key=%s", metric.Key())
+					assert.Len(t, metric.Values, test.expectedMetrics, "%d: %d", len(metric.Values), test.expectedMetrics)
+				}
+
+				assert.NoError(t, iter.Err())
+				assert.Equal(t, test.expectedNum, num)
+				assert.Equal(t, test.expectedChunks, counter)
+			})
+		})
+	}
+}
