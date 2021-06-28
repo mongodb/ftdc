0d7b003cf093ce50fed41b532ad42f4250369f21
edzh <edwinzzhou@gmail.com>
2021-06-28 13:52:18 -0400

    nit

--- t2.go
+++ t2.go
@@ -15,7 +15,7 @@ const (
 	max_samples int   = 300
 )
 
-// TranslateGenny exports the contents of a stream of genny ts.ftdc 
+// TranslateGenny exports the contents of a stream of genny ts.ftdc
 // chunks into cedar ftdc which is readable using t2. Translates
 // cumulative event driven metrics into metrics of one-second granularity.
 func TranslateGenny(ctx context.Context, iter *ChunkIterator, output io.Writer, actorOpName string) error {
